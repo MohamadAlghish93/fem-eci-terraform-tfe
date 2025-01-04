b39bab5a337cca1a27b5eb3112bfde97c6850c33
mhd_alghish <mohamadalghish1993@gmail.com>
2025-01-04 18:05:36 +0400

    feat: github setup workspace

--- locals.tf
+++ locals.tf
@@ -6,11 +6,20 @@ locals {
   }
 
   workspace = {
+    
     "fem-eci-workspace3" = {
       description    = "Example automation workspace for Terraform Cloud resources."
       execution_mode = "remote"
       project_id     = module.project["fem-eci-project3"].id
       vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
     }
+
+     "fem-eci-github" = {
+      description         = "Example automation workspace for GitHub resources."
+      execution_mode      = "local"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
+    }
+
   }
 }
