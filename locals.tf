f17f3d5dfc2e6779bdabf09343f4c905c35f49a8
mhd_alghish <mohamadalghish1993@gmail.com>
2025-01-04 22:53:54 +0400

    feat: change local var in github workspace

--- locals.tf
+++ locals.tf
@@ -16,7 +16,7 @@ locals {
 
      "fem-eci-github" = {
       description         = "Example automation workspace for GitHub resources."
-      execution_mode      = "local"
+      execution_mode      = "remote"
       project_id          = module.project["fem-eci-project3"].id
       vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
     }
